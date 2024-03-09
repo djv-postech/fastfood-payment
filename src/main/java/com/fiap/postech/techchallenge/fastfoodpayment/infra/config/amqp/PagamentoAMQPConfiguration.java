be643e920021876d3c9aeb9912f861707dffba83
LineNicacio <line.nicacio@hotmail.com>
2024-03-09 17:11:24 -0300

    fix: virtual host

--- src/main/java/com/fiap/postech/techchallenge/fastfoodpayment/infra/config/amqp/PagamentoAMQPConfiguration.java
+++ src/main/java/com/fiap/postech/techchallenge/fastfoodpayment/infra/config/amqp/PagamentoAMQPConfiguration.java
@@ -1,16 +1,10 @@
 package com.fiap.postech.techchallenge.fastfoodpayment.infra.config.amqp;
 
 
-import java.util.HashMap;
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Exchange;
-import org.springframework.amqp.core.ExchangeBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -18,10 +12,11 @@ import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
 
 @Configuration
 @RequiredArgsConstructor
