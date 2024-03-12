4be687deff50c7f686950a9ecb949d223706a938
LineNicacio <line.nicacio@hotmail.com>
2024-03-11 21:54:32 -0300

    fix: tests

--- src/test/java/com/fiap/postech/techchallenge/fastfoodpayment/FastfoodPaymentApplicationTest.java
+++ src/test/java/com/fiap/postech/techchallenge/fastfoodpayment/FastfoodPaymentApplicationTest.java
@@ -1,9 +1,7 @@
 package com.fiap.postech.techchallenge.fastfoodpayment;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FastfoodPaymentApplicationTests {
 
     @Test
